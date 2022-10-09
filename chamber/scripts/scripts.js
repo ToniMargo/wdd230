e49cf5119954d7296a294d7feffb8b003597cf68
ToniMargo <tonihmartinez@gmail.com>
2022-10-09 02:21:05 -0500

    Made W04 Assignment: Chamber Home Page Build

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,28 @@
+// Get current year and put it in the footer
+document.querySelector("#date1").textContent = new Date().getFullYear();
+
+// Get the last modification date and put it in the footer
+document.querySelector("#lastmod").textContent = "Last updated: " + new Date(document.lastModified);
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+
+
+// select the elements to manipulate (output to)
+const datefieldUK = document.querySelector(".todayDate"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefieldUK.innerHTML = fulldateUK;
