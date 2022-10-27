b1f38462570827caf218104d332d61db3df837e8
ToniMargo <tonihmartinez@gmail.com>
2022-10-26 22:37:55 -0500

    Made W07 Prepare Activity: Progressive Loading of Images.

---
+++ lesson7/scripts/lazyload.js
@@ -0,0 +1,42 @@
+// Get current year and put it in the footer
+document.querySelector("#date1").textContent = new Date().getFullYear();
+
+// Get the last modification date and put it in the footer
+document.querySelector("#lastmod").textContent = "Last updated: " + new Date(document.lastModified);
+
+
+//Lazyload images
+//----------------------------------------------------------------------------
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+
+    img.src = src;
+    img.removeAttribute("data-src");
+} 
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -500px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+//----------------------------------------------------------------------------
+  
