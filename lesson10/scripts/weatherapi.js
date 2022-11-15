73957b07b17226311434572c8058bd889cb412d5
ToniMargo <tonihmartinez@gmail.com>
2022-11-14 18:44:38 -0600

    W10 Weather API

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,38 @@
+// Get current year and put it in the footer
+document.querySelector("#date1").textContent = new Date().getFullYear();
+
+// Get the last modification date and put it in the footer
+document.querySelector("#lastmod").textContent = "Last updated: " + new Date(document.lastModified);
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=d8b80212a11f7cffaca6a1ced52c249f'
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
