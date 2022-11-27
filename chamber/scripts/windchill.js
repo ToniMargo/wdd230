e421bb6e272110f81edd74ae42df4f938fe46418
ToniMargo <tonihmartinez@gmail.com>
2022-11-26 23:57:44 -0600

    Deleted more unnecessary images

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -48,26 +48,3 @@ function displayResults(weatherData) {
         
         
 }
-
-/*Old windchill function *var jsonData
-
-$(function () {
-    
-    $.getJSON('https://api.openweathermap.org/data/2.5/weather?q=San-Antonio&APPID=d8b80212a11f7cffaca6a1ced52c249f', function(data) {
-        jsonData = data;
-        
-        temp = (jsonData.main.temp - 273.15) * 9/5 + 32;
-        wind = jsonData.wind.speed;
-        
-        windChill = (0.0817*(3.71*(Math.pow(wind, 0.5))+
-5.81-0.25*wind)*(temp-91.4)+91.4);
-
-        $('.temperature').text(temp.toFixed(1) + '°');
-        $('.windSpeed').text('Wind speed: ' + wind + ' mph');
-        if(temp<=50 && wind>3){
-        	$('.windChill').text('Wind chill: ' + windChill.toFixed(1) + ' °F');
-        } else {
-        	$('.windChill').text('Wind chill: N/A');
-        }
-    });
-});*/
